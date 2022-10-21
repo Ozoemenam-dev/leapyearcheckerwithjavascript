d5f1898085c0919ea971d51959d106ce0c23ef0c
Okpara Chidiebere Ozoemenam <114841298+Ozoemenam-dev@users.noreply.github.com>
2022-10-21 08:27:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// var year = prompt("Which year do you want to check? ")
+function isLeap(year) {
+    
+/**************Don't change the code above****************/    
+if (year % 4 == 0  && year % 100 != 0 || year % 400 == 0) {
+console.log("'Leap year'")
+}   else {
+  console.log("'Not leap year'")
+}
+    
+
+
+
+    
+
+/**************Don't change the code below****************/    
+
+}
+isLeap(1998)
